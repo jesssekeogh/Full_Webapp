135b93c429ddbbc2d451810df57a29e4bca609c7
Jesse Keogh <jessekeogh@jesses-air.home>
2021-06-23 22:12:27 +0100

    working on account

--- Webapp_Site_root/account/views.py
+++ Webapp_Site_root/account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+def account_register(request):
+
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            #set up email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject, message=message)
+    
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
+
